ef131d4ce37040dfb14575e945819270f949a6a5
Ali Yousefi <ali.visual.studio@gmail.com>
2022-12-23 14:51:31 +0330

    Add HttpHeadersConstants ContentLength

---
+++ src/CSharp/EasyMicroservice.Utilities.Tests/Constants/HttpHeadersConstantsTest.cs
@@ -0,0 +1,15 @@
+﻿using EasyMicroservice.Utilities.Constants;
+using Xunit;
+
+namespace EasyMicroservice.Utilities.Tests.Constants
+{
+    public class HttpHeadersConstantsTest
+    {
+        [Theory]
+        [InlineData(HttpHeadersConstants.ContentLength, "content-length")]
+        public void CheckValues(string value, string expect)
+        {
+            Assert.Equal(value, expect);
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservice.Utilities/Constants/HttpHeadersConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyMicroservice.Utilities.Constants
+{
+    /// <summary>
+    /// Http headers contants
+    /// </summary>
+    public class HttpHeadersConstants
+    {
+        /// <summary>
+        /// Content-Length header
+        /// </summary>
+        public const string ContentLength = "content-length";
+    }
+}
