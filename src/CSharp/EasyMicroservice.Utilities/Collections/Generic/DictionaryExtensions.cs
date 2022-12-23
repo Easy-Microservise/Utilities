c41d64bd04d3e51bfe5d0f7bbfe8fc876655a84c
Ali Yousefi <ali.visual.studio@gmail.com>
2022-12-23 14:34:51 +0330

    Add TryAddItem

---
+++ src/CSharp/EasyMicroservice.Utilities/Collections/Generic/DictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Extensions method helper for dictionary
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryAddItem<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+#if (NET45 || NETSTANDARD2_0)
+            if (dictionary.ContainsKey(key))
+                return false;
+            dictionary[key] = value;
+            return true;
+#else
+            return dictionary.TryAdd(key, value);
+#endif
+        }
+    }
+}
