dac0d6588301d62118e7577b3f115cb4acd1c5be
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-02 13:10:28 +0430

    Add support for StreamToBytesAsync #7

---
+++ src/CSharp/EasyMicroservices.Utilities.Tests/IO/StreamExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using EasyMicroservices.Utilities.IO;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyMicroservices.Utilities.Tests.IO
+{
+    public class StreamExtensionsTest
+    {
+        [Fact]
+        public async Task StreamToBytesAsync()
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            byte[] bytes = new byte[100];
+            for (int i = 0; i < 100; i++)
+            {
+                bytes[i] = (byte)i;
+                await memoryStream.WriteAsync(new byte[] { bytes[i] }, 0, 1);
+            }
+
+            var readBytes = await memoryStream.StreamToBytesAsync(100, 10);
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.Equal(readBytes[i], (byte)i);
+            }
+        }
+    }
+}
