63808a4e1300087c46cc5bb528929c9ea60fa4b1
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-04 11:27:41 +0430

    support for CopyStreamToStreamAsync

--- src/CSharp/EasyMicroservices.Utilities.Tests/IO/StreamExtensionsTest.cs
+++ src/CSharp/EasyMicroservices.Utilities.Tests/IO/StreamExtensionsTest.cs
@@ -24,5 +24,26 @@ namespace EasyMicroservices.Utilities.Tests.IO
                 Assert.Equal(readBytes[i], (byte)i);
             }
         }
+
+        [Fact]
+        public async Task CopyStreamToStreamAsync()
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            byte[] bytes = new byte[100];
+            for (int i = 0; i < 100; i++)
+            {
+                bytes[i] = (byte)i;
+                await memoryStream.WriteAsync(new byte[] { bytes[i] }, 0, 1);
+            }
+
+            using MemoryStream writeStream = new MemoryStream();
+
+            await memoryStream.CopyStreamToStreamAsync(writeStream, 100, 10);
+            var readBytes = writeStream.ToArray();
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.Equal(readBytes[i], (byte)i);
+            }
+        }
     }
 }
