90bf1e09ad0ecd1c86f691205989a1e257d4f8a4
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-08 14:06:19 +0430

    support for IStreamMiddleware

---
+++ src/CSharp/EasyMicroservices.Utilities/IO/Interfaces/IStreamMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.Utilities.IO.Interfaces
+{
+    /// <summary>
+    /// this Middleware help you to make a wrapper to use stream
+    /// for example compress a stream and encrypt it
+    /// </summary>
+    public interface IStreamMiddleware
+    {
+        /// <summary>
+        /// inner middleware
+        /// </summary>
+        public IStreamMiddleware InnerStreamMiddleware { get; set; }
+        /// <summary>
+        /// get stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public Task<Stream> GetStream(Stream stream);
+    }
+}
