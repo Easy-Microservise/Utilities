5160ab63988e00d7675247e06b91e2838d88bf2f
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-15 16:43:07 +0330

    add StringHelper

---
+++ src/CSharp/EasyMicroservices.Utilities/Text/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace System.Text
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StringHelper
+    {
+        /// <summary>
+        /// check the string is not null or empty
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool HasValue(this string str)
+        {
+            return !string.IsNullOrEmpty(str);
+        }
+
+        /// <summary>
+        /// check the string is null or empty
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+    }
+}
