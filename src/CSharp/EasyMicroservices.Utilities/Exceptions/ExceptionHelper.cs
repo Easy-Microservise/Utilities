91e133182185c7f28ac082cf5b26eb55f62a3add
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-15 16:43:33 +0330

    add  ExceptionHelper

---
+++ src/CSharp/EasyMicroservices.Utilities/Exceptions/ExceptionHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ExceptionHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="name"></param>
+        public static void ThrowIfNull<T>(this T source, string name)
+            where T : class
+        {
+            if (source == null)
+            {
+                if (name.HasValue())
+                {
+                    throw new NullReferenceException(name);
+                }
+                else
+                    throw new NullReferenceException(nameof(source));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="name"></param>
+        public static void ThrowIfNullOrEmpty<T>(this IEnumerable<T> source, string name = default)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                if (name.HasValue())
+                {
+                    throw new NullReferenceException($"{name} of type IEnumerable<{typeof(T).FullName}>");
+                }
+                else
+                    throw new NullReferenceException($"{nameof(source)} of type IEnumerable<{typeof(T).FullName}>");
+            }
+        }
+    }
+}
