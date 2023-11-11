473c10be80e66f5a2a37c2a7400ab8aa155df952
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-11 22:30:28 +0330

    Support for AmountContract

---
+++ src/CSharp/EasyMicroservices.Domain/Contracts/Common/AmountContract.cs
@@ -0,0 +1,19 @@
+﻿using EasyMicroservices.Domain.DataTypes;
+
+namespace EasyMicroservices.Domain.Contracts.Common
+{
+    /// <summary>
+    /// /
+    /// </summary>
+    public class AmountContract
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal Value { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public CurrencyCodeType CurrencyCode { get; set; }
+    }
+}
